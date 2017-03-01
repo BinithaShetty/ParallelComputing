06cf572d3700321f7b0875ba36492cfc31d06596
dell <binitharshetty@gmail.com>
2017-03-01 23:42:08 +0530

    first commit

---
+++ matrixSerial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+
+	int size = 750;
+    	float x[size][size];
+    	float y[size][size];
+    	float z[size][size];//Resultant matrix
+	//Initializing matrices
+    	for (int i = 0; i < size; ++i) {
+        	for (int j = 0; j < size; ++j) {
+            		x[i][j] = (float)i + j;
+            		y[i][j] = (float)i - j;
+            		z[i][j] = 0.0f;
+        	}
+    	}
+	//Computing the product
+    	for (int i = 0; i < size; ++i) {
+        	for (int j = 0; j < size; ++j) {
+            		for (int k = 0; k < size; ++k) {
+                		z[i][j] += x[i][k] * y[k][j];
+            		}
+        	}	
+    	}
+
+    	return 0;
+}
